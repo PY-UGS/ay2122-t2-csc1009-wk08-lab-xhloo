ff3c577000dfd6f2d7881f7cdda9aeaa8e976a35
xhloo <97490258+xhloo@users.noreply.github.com>
2022-03-15 12:22:04 +0800

    wk08 laboratory

--- Q1/CircleWithException.java
+++ Q1/CircleWithException.java
@@ -5,42 +5,42 @@ public class CircleWithException
 	
 	
 	public CircleWithException() 
-    {
+    	{
 		this.radius = radius;
 	}
 	
 	public double getRadius() 
-    {
+    	{
 		return radius;
 	}
 	
 	public void setRadius(double radius) 
-    {
+    	{
 		if (radius < 1) 
-        {
+        	{
 			throw new IllegalArgumentException("The radius must be a positive number");
 		}
-        else 
-        {
+        	else 
+        	{
 			this.radius = radius;
 		}
 	}
 	
 	public double getArea() throws Exception 
-    {
+    	{
 		double area = PI * this.radius * this.radius;
 		if (area >1000) 
-        {
+        	{
 			throw new Exception("The area must not be bigger than 1000");
 		}
-        else 
-        {
+        	else 
+        	{
 			return PI * this.radius * this.radius;
 		}
 	}
 	
 	public double getDiameter() 
-    {
+    	{
 		return this.radius + this.radius;
 	}
 }
