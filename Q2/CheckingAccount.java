135404cae4f858f097391904686835f50758031b
xhloo <97490258+xhloo@users.noreply.github.com>
2022-03-15 12:16:51 +0800

    Update CheckingAccount.java

--- Q2/CheckingAccount.java
+++ Q2/CheckingAccount.java
@@ -4,13 +4,13 @@ public class CheckingAccount
 	private String accountNumber;
 	
 	public CheckingAccount(double balance, String accountNumber) 
-    {
+    	{
 		this.balance = balance;
 		this.accountNumber = accountNumber;
 	}
 
 	public void deposit(double amount) throws InsufficientFundsException
-    {
+    	{
 		if (amount > 0)
 		{
 			this.balance += amount;
@@ -23,29 +23,29 @@ public class CheckingAccount
 	}
 	
 	public void withdraw(double amount) throws InsufficientFundsException 
-    {
+    	{
 		if (amount > balance) 
-        {
+       		{
 			throw new InsufficientFundsException(amount - balance);
 		}
 		if (amount <= 0)
 		{
 			throw new IllegalArgumentException("The withdrawal amount is invalid");
 		}
-        else 
-        {
+        	else 
+        	{
 			this.balance -= amount;
 			System.out.println("The balance after withdraw is: $" + this.balance);
 		}
 	}
 
 	public double getBalance() 
-    {
+    	{
 		return balance;
 	}
 
 	public String getNumber() 
-    {
+    	{
 		return accountNumber;
 	}
 }
