669e68f4441fe35d45e9486d4e379c0847cacb2b
xhloo <97490258+xhloo@users.noreply.github.com>
2022-03-15 12:17:59 +0800

    Update BankDemoTest.java

--- Q2/BankDemoTest.java
+++ Q2/BankDemoTest.java
@@ -6,21 +6,21 @@ public class BankDemoTest
     {
         try(Scanner sc = new Scanner(System.in))
         {
-			CheckingAccount account = new CheckingAccount(0, "12345678");
-			System.out.print("Deposit amount: ");
-			double amount = sc.nextDouble();
-			account.deposit(amount);
-			System.out.print("Withdrawal amount: ");
-			amount = sc.nextDouble();
-			account.withdraw(amount);
-		}
+		CheckingAccount account = new CheckingAccount(0, "12345678");
+		System.out.print("Deposit amount: ");
+		double amount = sc.nextDouble();
+		account.deposit(amount);
+		System.out.print("Withdrawal amount: ");
+		amount = sc.nextDouble();
+		account.withdraw(amount);
+	}
         catch(InsufficientFundsException e)
         {
-			System.out.println("Sorry but your account is short by: $" + e.getAmount());
-		}
-		catch(IllegalArgumentException e)
-		{
-			System.out.println(e.getMessage());
-		}
+		System.out.println("Sorry but your account is short by: $" + e.getAmount());
+	}
+	catch(IllegalArgumentException e)
+	{
+		System.out.println(e.getMessage());
+	}
     }
 }
